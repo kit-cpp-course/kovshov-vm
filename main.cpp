c2c02fa80e7aec1bc68722ffcaaffb2a798d6ef7
Valery Kovshov <34447913+prohladenn@users.noreply.github.com>
2018-12-28 10:05:08 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,23 +1,99 @@
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+
 #include <iostream>
-#include "jsonSimple.h"
 
-int main() {
+using namespace std;
+using namespace boost::property_tree;
 
-    string input = "{\"menu\": {\n"
-                   "  \"id\": \"file\",\n"
-                   "  \"value\": \"File\",\n"
-                   "  \"popup\": {\n"
-                   "    \"menuitem\": [\n"
-                   "      {\"value\": \"New\", \"onclick\": \"CreateNewDoc()\"},\n"
-                   "      {\"value\": \"Open\", \"onclick\": \"OpenDoc()\"},\n"
-                   "      {\"value\": \"Close\", \"onclick\": \"CloseDoc()\"}\n"
-                   "    ]\n"
-                   "  }\n"
-                   "}}";
+int main(int argc, char *argv[]) {
+	
+	cout << argc << endl;
 
-    JSON json(input);
+	for (int i = 0; i < argc; i++)
+		cout << argv[i] << endl;
 
-    cout << json.toXML() << endl;
-
-    return 0;
+	switch (argc)
+	{
+		case 2 :
+		{
+			string input = argv[1];
+			cout << "Vvedeno dve peremenie\n";
+			if (input.substr(input.length() - 3, 3) == "xml") {
+				cout << "Input xml\n";
+				ptree pt;
+				read_xml(input, pt);
+				try
+				{
+					write_json(cout, pt);
+				}
+				catch (const std::exception&)
+				{
+					cout << "Xml parsing exception\n";
+				}
+			}
+			else if (input.substr(input.length() - 4, 4) == "json")
+			{
+				cout << "Input json\n";
+				ptree pt;
+				read_json(input, pt);
+				try
+				{
+					write_xml(cout, pt);
+				}
+				catch (const std::exception&)
+				{
+					cout << "Json parsing exception\n";
+				}
+			}
+			else
+			{
+				cout << "Input exception. Plece write path file to convert\n";
+			}
+			break;
+		}
+		case 3 :
+		{
+			string input = argv[1];
+			string output = argv[2];
+			cout << "Vvedeno tri peremenie\n";
+			if (input.substr(input.length() - 3, 3) == "xml") {
+				cout << "Input xml + Output\n";
+				ptree pt;
+				read_xml(input, pt);
+				try
+				{
+					write_json(output, pt);
+				}
+				catch (const std::exception&)
+				{
+					cout << "Xml parsing exception\n";
+				}
+			}
+			else if (input.substr(input.length() - 4, 4) == "json")
+			{
+				cout << "Input json + Output\n";
+				ptree pt;
+				read_json(input, pt);
+				try
+				{
+					write_xml(output, pt);
+				}
+				catch (const std::exception&)
+				{
+					cout << "Json parsing exception\n";
+				}
+			}
+			else
+			{
+				cout << "Input exception. Plece write path file to convert.\n";
+			}
+			break; 
+		}
+		default: {
+			cout << "Input exception. Plece write path file to convert.\n";
+		}		
+	}
+	return 0;
 }
